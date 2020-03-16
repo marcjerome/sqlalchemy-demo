ff1869797e05fcd29f1fb9416c3f678bd9576f70
Marc Jerome <mjt.eight0five@gmail.com>
2020-03-17 07:59:28 +0800

    Added tag feature

--- gallery.py
+++ gallery.py
@@ -2,7 +2,7 @@ import os
 
 from flask import Flask, request, redirect, url_for, flash, render_template, send_from_directory
 from werkzeug.utils import secure_filename
-from models import db, init_db, Image
+from models import db, init_db, Image, Comment, Tag
 
 UPLOAD_FOLDER = os.path.abspath(os.getcwd()) + '/uploads'
 ALLOWED_EXTENSIONS = {'jpg', 'png', 'jpeg', 'gif'}
@@ -33,7 +33,16 @@ def upload_file():
                 image.save(os.path.join(app.config['IMAGE_UPLOADS'], image.filename))
                 title = request.form.get('title')
                 filename = image.filename 
+                
                 db_image = Image(title, filename)
+                if 'tag' in request.form:
+                    tags = request.form.getlist('tag')
+                    tag_list = Tag.query.filter(Tag.tag_name.in_(tags))
+
+                    for tag in tag_list:
+                        db_image.tags.append(tag)
+
+
                 db.session.add(db_image)
                 db.session.commit()
 
@@ -44,27 +53,59 @@ def upload_file():
             return "No image uploaded!"
 
 
+@app.route('/image/delete/<filename>', methods=['POST'])
+def delete_image(filename):
+    if request.method == 'POST':
+        if 'delete' in request.form:
+            image = Image.query.filter_by(image_filename=filename).first()
+            os.remove(os.path.join(app.config['IMAGE_UPLOADS'], filename))
+            db.session.delete(image)
+            db.session.commit()
+            return redirect(url_for('index'))
+    return "Bad"
+
+@app.route('/image/comment/<filename>', methods=['POST'])
+def add_comment(filename):    
+    if request.method == "POST":
+        if 'add_comment' in request.form:
+            image = Image.query.filter_by(image_filename=filename).first()
+            comment_text = request.form.get('comment_text')
+            image.comments.append(Comment(comment_text))
+            db.session.add(image)
+            db.session.commit()
+            return redirect(url_for('index'))
+
+
+@app.route('/image/edittitle/<filename>', methods=['POST', 'GET'])
+def edit_title(filename):  
+    print('Im here in edit title function!')
+    if 'change_page' in request.form:
+        print('Im here in change page!')
+        return render_template('title.html', filename=filename)
+
+    elif 'change_title' in request.form:
+        print('Im here in change title!')
+        image = Image.query.filter_by(image_filename=filename).first()
+        image.image_title = request.form.get('title')
+        db.session.add(image)
+        db.session.commit()
+        return redirect(url_for('index'))
+
+
+@app.route('/tag', methods=["POST"])
+def add_tag():
+    if request.method == "POST":
+        if 'add_tag' in request.form:
+            tag_name = request.form.get('tag_name')
+            tag = Tag(tag_name)
+            db.session.add(tag)
+            db.session.commit() 
+            
+    return redirect(url_for('index'))
+
 
 @app.route('/')
 def index():
     images = Image.query.all()
-    return render_template('index.html', images=images)
-
-    '''
-        Image 
-        Title 
-        Image Tags
-        Comments
-
-        Image 
-            image_title
-        Tags
-            tag_name
-        Comments 
-            comment_Text
-        
-        * An image can have many tags and a tag can have many images
-        * An image can have many comments and a comment belongs to one image
-        
-
-    '''
+    tags = Tag.query.all()
+    return render_template('index.html', images=images, tags=tags)
--- models.py
+++ models.py
@@ -19,8 +19,8 @@ class Image(db.Model):
     image_title = db.Column(db.String(120))
     image_filename = db.Column(db.String(200), unique = True, nullable=False)
 
-    comments = db.relationship('Comment', backref='image', lazy=True)
-    tags = db.relationship('Tag', secondary=tags, lazy='subquery', backref=db.backref('image', lazy=True))
+    comments = db.relationship('Comment', cascade='all,delete', backref='image', lazy=True)
+    tags = db.relationship('Tag', cascade='all,delete',  secondary=tags, lazy='subquery', backref=db.backref('image', lazy=True))
 
     def __init__(self, title, filename):
         self.image_title = title
@@ -33,9 +33,14 @@ class Tag(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     tag_name = db.Column(db.String(120), unique=True)
 
+    def __init__(self, tag_name):
+        self.tag_name = tag_name
 class Comment(db.Model):
     __tablename__ = 'Comment'
 
     id = db.Column(db.Integer, primary_key=True)
     image_id = db.Column(db.Integer, db.ForeignKey('Image.id'), nullable=False)
-    comment_text = db.Column(db.String(120)) 
+    comment_text = db.Column(db.String(120))    
+
+    def __init__(self, comment):
+        self.comment_text = comment        
